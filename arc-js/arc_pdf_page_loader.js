4f98ead6145b34883f41ad412ab4493a08be3e66
CloudyChris <damianfate@gmail.com>
2018-01-22 13:07:29 +0200

    almost done dynamic page integration

---
+++ arc-js/arc_pdf_page_loader.js
@@ -0,0 +1,13 @@
+
+function load_dynamic_page(pdf_path){
+
+  $("#arc_main").load("content/pages/dynamic/stdidp.html");
+  $("#stdidp_div").load(pdf_path);
+
+}
+
+$(document).ready(function (){
+
+    
+
+});
