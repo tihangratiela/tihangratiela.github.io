ad3129a31ec5d741aefea4641225a71da7fd6e9a
CloudyChris <damianfate@gmail.com>
2018-01-23 20:46:47 +0200

    a little step for man, a huge step for mankind

--- arc-js/arc_pdf_page_loader.js
+++ arc-js/arc_pdf_page_loader.js
@@ -1,13 +1,46 @@
 
-function load_dynamic_page(pdf_path){
+function menu_constructor_stage_2(meid, mename){
 
-  $("#arc_main").load("content/pages/dynamic/stdidp.html");
-  $("#stdidp_div").load(pdf_path);
+  var mediv = document.createElement("div");
+  var me_h = document.createElement("a");
 
+
+  mediv.setAttribute("class", "menu_entry");
+  mediv.setAttribute("id",meid);
+  me_h.setAttribute("href","content/pages/dynamic/"+meid+".pdf");
+  me_h.setAttribute("target", "_blank");
+  me_h.innerHTML= mename;
+
+  mediv.appendChild(me_h);
+
+  $("#arc_side").append(mediv);
+
+  return 0;
+
+}
+
+function menu_constructor_stage_1(xml_file_path){
+
+  $.ajax({
+  type: "GET",
+  url: xml_file_path,
+  dataType: "xml",
+  success: function(xml) {
+      $(xml).find('entry').each(function(){
+          var mename = $(this).find('mename').text();
+          var meid = $(this).find('meid').text();
+          var pdf_path = $(this).find('pdf_path').text();
+          menu_constructor_stage_2(meid, mename);
+      });
+  }
+});
+
+return 0;
 }
 
 $(document).ready(function (){
 
-    
+  menu_constructor_stage_1("content/pages/dynamic/dpages.xml");
+
 
 });
